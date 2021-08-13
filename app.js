55812aaa264c8027a86849d50a0eea470d560dd9
Brandon Rimes <77939170+BrandonRimes@users.noreply.github.com>
2021-08-13 14:25:11 -0400

    step-eleven

--- app.js
+++ app.js
@@ -111,22 +111,20 @@ const newObject = array => ({
 let sum = (a, b, c, d) => a + b + c + d;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-console.log(sum(1, 2, 3, 4));
+// console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
-  return {
+let objectLit = () => ({
     key1: 'value1',
     key2: 'value2',
     key3: 'value3',
-  };
-};
+});
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
+let sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
@@ -136,9 +134,7 @@ let sumAndProduct = function(a, b) {
 // console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
+let message = name => `Hello, ${name}!`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(message('Allie'));
@@ -197,8 +193,8 @@ Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// - "this" is the student object 
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// - "this" is the document object "Window"
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// - Arrow functions do not have thier own bindings to "this" and inherit it's value from the nearest non-arrow enclosure, in this case the global window object.
